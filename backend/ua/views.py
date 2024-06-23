d2678c1f6056423bb0212507a82a3a9bae527798
Ashish Pandian <ashishpandian@berkeley.edu>
2024-06-22 18:33:37 -0700

    django unchained

--- backend/ua/views.py
+++ backend/ua/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import googlemaps
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import requests
 
 # Create your views here.
+
+GOOGLE_MAPS_API_KEY = ""
+def traffic_request(request):
+    if request.method != 'GET':
+        requests.get('https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=visualization&v=weekly')
+        data = json.loads(request.body.decode())
+        zoom = data.get('zoom')
+        center = data.get('center') # or 'center/lat'
+        lat = center.lat
+        lng = center.lng
+        return render(request, "users.html")
+    else: # current GET request
+        # placeholder code
+        response = requests.get('https://jsonplaceholder.typicode.com/users')
+
+        #convert reponse data into json
+        users = response.json()
+        print(users)
+        for u in users:
+            print(u.get('name'))
+        # print(users[0].get('name'))
+        return render(request, "users.html", {'users': users})
+        return HttpResponse("Hi")
+
+
