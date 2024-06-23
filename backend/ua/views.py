51d584f7274a9cc4af03224c1f413ecb00561408
PrathamRathi <pratham.02rathi@gmail.com>
2024-06-22 21:36:30 -0700

    removed users

--- backend/ua/views.py
+++ backend/ua/views.py
@@ -1,34 +1,60 @@
 from django.shortcuts import render
-from django.shortcuts import render, HttpResponse
-import googlemaps
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.http import QueryDict
+from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
+from ML_models.analyze_in_request import requestPipeline
 import json
-import requests
+from .models import *
+from .serializers import *
+# random change
+@csrf_exempt
+def post(request):
+        if request.method == 'POST':
+            data = json.loads(request.body.decode())
+            objective = data.get('objective')
+            overview = data.get('overview')
+            subject = data.get('subject')
+            students = Student.objects.all()
+            grades = Grade.objects.all()
+            grade_map = {}
+            for grade in grades:
+                if grade.student.uuid not in grade_map:
+                    grade_map[grade.student.uuid] = [{'grade': grade.percentage, 'date': grade.date}]
+                else:
+                    grade_map[grade.student.uuid].append({'grade': grade.percentage, 'date': grade.date})
+            response = {}
+            for student in students:
+                if student.disability != "None":
+                    iep = IEP.objects.all().filter(student=student.uuid)[0]
+                    text, status = requestPipeline(subject, objective, overview, iep.accommodation, 
+                                                student.uuid, grade_map, student.standard, student.disability)
+                    if not status:
+                        if LessonPlan.objects.last() == None:
+                            lp = LessonPlan(name="initial plan", objectives=objective, overview=overview, subject=subject, present_date="02/19/2023")
+                            lp.save()
+                        lpsa = LpsAccommodation(lesson_plan=LessonPlan.objects.last(), student=student, accommodation = text)
+                        lpsa.save()
+                    response[str(student.uuid)] = text         
+            return JsonResponse(response)
 
-# Create your views here.
+class StudentView(viewsets.ModelViewSet):
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
 
-GOOGLE_MAPS_API_KEY = ""
-def traffic_request(request):
-    if request.method != 'GET':
-        requests.get('https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=visualization&v=weekly')
-        data = json.loads(request.body.decode())
-        zoom = data.get('zoom')
-        center = data.get('center') # or 'center/lat'
-        lat = center.lat
-        lng = center.lng
-        return render(request, "users.html")
-    else: # current GET request
-        # placeholder code
-        response = requests.get('https://jsonplaceholder.typicode.com/users')
+class LessonPlanView(viewsets.ModelViewSet):
+    serializer_class = LessonPlanSerializer
+    queryset = LessonPlan.objects.all()
 
-        #convert reponse data into json
-        users = response.json()
-        print(users)
-        for u in users:
-            print(u.get('name'))
-        # print(users[0].get('name'))
-        return render(request, "users.html", {'users': users})
-        return HttpResponse("Hi")
+class IepView(viewsets.ModelViewSet):
+    serializer_class = IepSerializer
+    queryset = IEP.objects.all()
 
 
+class LpsaView(viewsets.ModelViewSet):
+    serializer_class = LpsaccommodationSerializer
+    queryset = LpsAccommodation.objects.all()
+
+class GradeView(viewsets.ModelViewSet):
+    serializer_class = GradeSerializer
+    queryset = Grade.objects.all()
